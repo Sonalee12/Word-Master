e6239620175a2e5d520b5677b41509079fb7f389
Sonalee12 <159402389+Sonalee12@users.noreply.github.com>
2024-10-21 11:16:37 +0545

    Updates

---
+++ precache-manifest.1c9f7faf0ab49212c84dd1881a1be0a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16e762ed15485c1256fed3d0f329838b",
+    "url": "/Word-Master/index.html"
+  },
+  {
+    "revision": "49155adc228ae7b636ef",
+    "url": "/Word-Master/static/css/main.59f0975b.chunk.css"
+  },
+  {
+    "revision": "5b8e05b2f58a7be6de68",
+    "url": "/Word-Master/static/js/2.631a136b.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/Word-Master/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "49155adc228ae7b636ef",
+    "url": "/Word-Master/static/js/main.895b86c9.chunk.js"
+  },
+  {
+    "revision": "d334f3aa855cd31a2114",
+    "url": "/Word-Master/static/js/runtime~main.c7c2d600.js"
+  }
+]);
